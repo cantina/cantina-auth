abdbc05eb86c46858f0f255da75d5294a9c2866c
Carlos Rodriguez <carlos@s8f.org>
2012-09-27 11:28:32 -0700

    restore destination redirect changes lost in 2.x upgrade

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * Auth cantina plugin.
  */
 
-var app = require('cantina'),
-    passport = require('passport');
+var app = require('cantina')
+  , passport = require('passport')
+  , parseUrl = require('url').parse
 
 app.conf.add({
   auth: {
@@ -12,7 +13,7 @@ app.conf.add({
   }
 });
 
-app.on('init', function() {
+app.on('init', function () {
   var conf = app.conf.get('auth');
 
   // Applications MUST listen for serialization events.
@@ -20,39 +21,34 @@ app.on('init', function() {
   passport.deserializeUser(app.invoke.bind(app, 'auth:deserialize'));
 
   // Add auth middleware.
-  app.middleware.add(extendRequest());
+  app.middleware.add(function (req, res, next) {
+    // Passport on express's res.redirect() so insert shim for that.
+    res.redirect = function (url, status) {
+      if (req.session.destination && (!req.query.destination || req.url.indexOf(conf.logoutPath) === 0)) {
+        url = req.session.destination;
+        delete req.session.destination;
+      }
+      res.writeHead(status || 302, {'Location': url});
+      res.end();
+    };
+    // Also depends on req.query. sigh.
+    if (!req.query) {
+      req.query = ~req.url.indexOf('?') ? parseUrl(req.url, true).query : {};
+    }
+    // Add support for ?destination=/some-path
+    if (req.query.destination && !~req.query.destination.indexOf('://')) {
+      req.session.destination = req.query.destination;
+    }
+    next();
+  });
   app.middleware.add(passport.initialize());
   app.middleware.add(passport.session());
 
   // Add a logout route.
-  app.middleware.get(conf.logoutPath, function(req, res) {
+  app.middleware.get(conf.logoutPath, function (req, res) {
     req.logOut();
     res.redirect(conf.logoutRedirect);
   });
 
   app.passport = passport;
 });
-
-/**
- * Passport relies on req being descended from http.IncomingMessage, and
- * modifies that prototype with custom methods. Bad, bad, bad!
- * This little hack mirrors those methods on req, since union uses a custom set
- * of classes that don't inherit from http.IncomingMessage.
- * Passport also relies on express's res.redirect() so insert shim for that too.
- */
-function extendRequest () {
-  var reqPrototype = require('http').IncomingMessage.prototype;
-  return function(req, res, next) {
-    var methods = ['logIn', 'logOut', 'isAuthenticated', 'isUnauthenticated'], prop;
-    for (var i in methods) {
-      prop = methods[i];
-      req[prop] = reqPrototype[prop].bind(req);
-    }
-    res.redirect = (function(url, status) {
-      this.statusCode = status || 302;
-      this.setHeader('Location', url);
-      this.end();
-    }).bind(res);
-    next();
-  };
-}
